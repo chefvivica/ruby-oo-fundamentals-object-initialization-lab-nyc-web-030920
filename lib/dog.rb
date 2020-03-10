842d8c40489937793348c702a55e8a87553df757
Vivica Ma <vivicacongma@gmail.com>
2020-03-10 15:44:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed = mutt)
     @name = name 
+    
   end 
   
   def name
     @name 
   end 
   
-  def initialize(breed = mutt)
+  def initialize(b)
     @breed = breed 
   end 
   
